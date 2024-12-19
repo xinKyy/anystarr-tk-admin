9a7b33be668ef5b3d86d18e78e1d8fd0c8ddaf91
xink <zyhh1611054604@gmail.com>
2024-12-19 12:29:03 +0800

    fix：add date

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fbb937650eaff44c26861288dee25d3.js"
+  "/precache-manifest.5a4ba54e96185d1bcc4394158ce6e22a.js"
 );
 
 self.addEventListener('message', (event) => {
--- build/static/js/0.e4664a07.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-admin"]=this["webpackJsonpreact-admin"]||[]).push([[0],{194:function(a,n,c){}}]);
-//# sourceMappingURL=0.e4664a07.chunk.js.map
